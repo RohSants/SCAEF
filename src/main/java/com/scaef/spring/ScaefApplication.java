4b1dda6c5e4c5222f57c00eceee150db5825cd9f
Roh <2019324007@ifam.edu.br>
2021-10-02 22:57:34 -0400

    Aplicações java foram renomeadas

--- src/main/java/com/scaef/spring/ScaefApplication.java
+++ src/main/java/com/scaef/spring/ScaefApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class ScaefApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(ScaefApplication.class, args);
 	}
 
 }
