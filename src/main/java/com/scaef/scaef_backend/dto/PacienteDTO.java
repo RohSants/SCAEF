0f38170a6c1d7397046baff56c5f7e55678df44c
Roh <2019324007@ifam.edu.br>
2021-10-17 19:50:48 -0400

    update

--- src/main/java/com/scaef/scaef_backend/dto/PacienteDTO.java
+++ src/main/java/com/scaef/scaef_backend/dto/PacienteDTO.java
@@ -1,7 +1,7 @@
 package com.scaef.scaef_backend.dto;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+/*import javax.validation.constraints.NotNull;*/
 import javax.validation.constraints.Size;
 
 /*Projeto Lombok*/
@@ -29,38 +29,4 @@ public class PacienteDTO {
     @NotBlank
     @Size(max = 11)
     private String cpf;
-
-    /*@NotBlank
-    @Size(max = 10)
-    private String rg;*/
-    
-    @NotBlank
-    @Size(max = 10)
-    private String dNasc;
-    
-    /*@Size(max = 95)
-    private String email;
-    
-    @NotBlank
-    @Size(max = 26)
-    private String cida;
-    
-    @NotBlank
-    @Size(max = 80)
-    private String endr;
-    
-    @Size(max = 50)
-    private String comp;
-    
-    @NotNull
-    private long cep;
-    
-    @NotNull
-    private int num;
-    
-    @NotNull
-    private long celular;
-    
-    @NotNull
-    private long fone;*/
 }
