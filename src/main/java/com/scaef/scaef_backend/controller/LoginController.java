c07e9f413c32b67b7b12996c2688c42dd5fa88d3
Paula Monteiro <84688579+PaulaMonteiro@users.noreply.github.com>
2021-11-01 18:31:39 -0400

    LoginController

--- src/main/java/com/scaef/scaef_backend/controller/LoginController.java
+++ src/main/java/com/scaef/scaef_backend/controller/LoginController.java
@@ -3,18 +3,51 @@ package com.scaef.scaef_backend.controller;
 
 
 
+
+import com.scaef.scaef_backend.model.Login;
+import com.scaef.scaef_backend.repository.LoginRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/login")
-    public String login(){
-        return "login";
-    }
+    @Autowired
+    private LoginRepository repo;
 
-    
+        @GetMapping("/login")
+       
+        public ModelAndView Login(Login login){
+            ModelAndView mv = new ModelAndView();
+            mv.setViewName("/login");
+            mv.addObject("login", new Login());
+            return mv;
+        }  
+        
+        @PostMapping("logar")
+        public String Logar(Model model, String senha, String email){
+            Login login = this.repo.Login(email, senha);
+            if (login != null){
+                return "redirect:/";
+            }
+            model.addAttribute("erro", "email ou senha inválidos");
+            return "login";
+        }
+
+       /* ModelAndView Logar(Login login){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("redirect:/Login/LoginList");
+        loginrepo.save(login);
+        return mv;
+    }*/
+
+
 
 }
+    
+
--- src/main/java/com/scaef/scaef_backend/repository/LoginRepository.java
+++ src/main/java/com/scaef/scaef_backend/repository/LoginRepository.java
@@ -3,10 +3,14 @@ package com.scaef.scaef_backend.repository;
 import com.scaef.scaef_backend.model.Login;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 
 
 public interface LoginRepository extends JpaRepository<Login, Long>{
-    
+
+    @Query(value="select * from login where email: email and senha: senha",nativeQuery = true)
+    public Login Login(String email, String senha);
+     
 }
